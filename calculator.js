ffa4fb8b104c19f6df33fbebfed7530d0a1c99e8
LucasKubo <lucaskbk2@gmail.com>
2021-09-25 10:57:38 -0300

    Adicionando função de subtração // Correção na exibição do operador de multiplicação

--- calculator.js
+++ calculator.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const display = document.getElementById('display');//tela
+const display = document.getElementById('display'); //tela
 //id *= 'string' --> seleciona todos os ids que contenham a string 'string'
-const numeros = document.querySelectorAll('[id*=tecla]');//numeros
-const operadores = document.querySelectorAll('[id*=operador]');//numeros
+const numeros = document.querySelectorAll('[id*=tecla]'); //numeros
+const operadores = document.querySelectorAll('[id*=operador]'); //numeros
 
 //variavel boolean para distinguir os numeros das operações
 let novoNumero = true;
@@ -17,38 +17,45 @@ const operacaoPendente = () => operador != undefined;
 
 //função que realiza as operações
 const calcular = () => {
-    if(operacaoPendente()){
-        const numeroAtual = parseFloat(display.textContent);
-        novoNumero = true;
-        if(operador = '+'){
-            atualizarDisplay(numeroAnterior + numeroAtual);
-        }
+  if (operacaoPendente()) {
+    const numeroAtual = parseFloat(display.textContent);
+    novoNumero = true;
+    switch (operador) {
+      case '+':
+        atualizarDisplay(numeroAnterior + numeroAtual);
+        break;
+      case '-':
+        atualizarDisplay(numeroAnterior - numeroAtual);
+      default:
+        break;
     }
-}
+  }
+};
 
 //função que atualiza o display de acordo com os botões clicados
 const atualizarDisplay = (texto) => {
-    if (novoNumero){
-        display.textContent = texto;
-        novoNumero = false;
-    }else{
-        display.textContent += texto;
-    }
-}
+  if (novoNumero) {
+    display.textContent = texto;
+    novoNumero = false;
+  } else {
+    display.textContent += texto;
+  }
+};
 
 //função que é disparada ao clique de um número
 const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
 //adicionar evento de clique em todos os numeros
-numeros.forEach (numero => numero.addEventListener('click',inserirNumero));
-
+numeros.forEach((numero) => numero.addEventListener('click', inserirNumero));
 
 const selecionarOperador = (event) => {
-    if(!novoNumero){
-        calcular();
-        novoNumero = true;
-        operador = event.target.textContent;
-        numeroAnterior = parseFloat(display.textContent);
-    }
-}
+  if (!novoNumero) {
+    calcular();
+    novoNumero = true;
+    operador = event.target.textContent;
+    numeroAnterior = parseFloat(display.textContent);
+  }
+};
 //adicionar evento de clique em todos os operadores
-operadores.forEach (operador => operador.addEventListener('click',selecionarOperador));
+operadores.forEach((operador) =>
+  operador.addEventListener('click', selecionarOperador)
+);
